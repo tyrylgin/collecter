c78b29e0132d77e18736684baa38462bbb38b3c4
tyrylgin <ity.dim@gmail.com>
2021-12-22 03:11:38 +0300

    implement storage for metrics

---
+++ internal/metrics/storage.go
@@ -0,0 +1,40 @@
+package metrics
+
+import (
+	"log"
+	"sync"
+)
+
+type Storage struct {
+	metrics map[string]Metric
+	mutex   sync.RWMutex
+}
+
+var DefaultStorage = &Storage{metrics: make(map[string]Metric)}
+
+func Register(name string, i interface{}) {
+	DefaultStorage.mutex.Lock()
+	defer DefaultStorage.mutex.Unlock()
+
+	// If metric with same name registered, ignore
+	if _, ok := DefaultStorage.metrics[name]; ok {
+		return
+	}
+
+	switch i.(type) {
+	case Gauge, Counter:
+		DefaultStorage.metrics[name] = i.(Metric)
+	default:
+		log.Fatal("unknown metric type, allowed MetricTypeCounter and MetricTypeGauge")
+		return
+	}
+}
+
+func Each(f func(string, Metric)) {
+	DefaultStorage.mutex.RLock()
+	defer DefaultStorage.mutex.RUnlock()
+
+	for name, metric := range DefaultStorage.metrics {
+		f(name, metric)
+	}
+}
